7d161bf29ddb4e8eaacc6cf0c88f8c8f74c0eb55
antony lulciuc <antony.lulciuc@gmail.com>
2018-02-28 00:08:11 -0500

    t

--- src/test/java/com/revature/pages/NewTest.java
+++ src/test/java/com/revature/pages/NewTest.java
@@ -1,12 +1,10 @@
 package com.revature.pages;
 
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 
 public class NewTest {
